e5c6afb9d2c7665126ca04bad59ee02dc0d808a9
Trevor Specht <trevor.specht@mapbox.com>
2019-02-10 00:29:08 -0500

    program9 and program8 cleaned up

--- program8.js
+++ program8.js
@@ -1,8 +1,7 @@
 var http = require('http');
 var url = process.argv[2];
-// var body = '';
 
-var req = http.request(url, function (res) {
+var req = http.get(url, function (res) {
   var body = '';
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
@@ -17,24 +16,3 @@ var req = http.request(url, function (res) {
 req.on('error', function (e) {
   console.log(e.message);
 });
-
-req.end();
-
-
-// var req = http.request(options, function (res) {
-//   req.on('response', function (res) {
-//     res.setEncoding('utf8');
-//     res.on('data', function (chunk) {
-//       body += chunk;
-//     });
-//     res.on('end', function () {
-//       console.log(body);
-//     });
-//   });
-// });
-//
-// req.on('error', function (e){
-//   console.log(e.message);
-// });
-//
-// req.end();
